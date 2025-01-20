8cf10e3153508f056010fdc42987eac84f85156e
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-01-20 16:11:27 -0600

    Update DriveEncoderConstants.java

--- src/main/java/com/pedropathing/localization/constants/DriveEncoderConstants.java
+++ src/main/java/com/pedropathing/localization/constants/DriveEncoderConstants.java
@@ -11,33 +11,33 @@ import com.pedropathing.localization.Encoder;
 public class DriveEncoderConstants {
 
     /** The number of inches per ticks of the encoder for forward movement
-     *  Default Value: 1 */
+     * Default Value: 1 */
     public static double forwardTicksToInches = 1;
 
     /** The number of inches per ticks of the encoder for lateral movement (strafing)
-     *  Default Value: 1 */
+     * Default Value: 1 */
     public static double strafeTicksToInches = 1;
 
     /** The number of inches per ticks of the encoder for turning
-     *  Default Value: 1 */
+     * Default Value: 1 */
     public static double turnTicksToInches = 1;
 
     public static double robot_Width = 1;
     public static double robot_Length = 1;
 
     /** The direction of the left front encoder
-     *  Default Value: Encoder.REVERSE */
+     * Default Value: Encoder.REVERSE */
     public static double leftFrontEncoderDirection = Encoder.REVERSE;
 
     /** The direction of the right front encoder
-     *  Default Value: Encoder.FORWARD */
+     * Default Value: Encoder.FORWARD */
     public static double rightFrontEncoderDirection = Encoder.FORWARD;
 
     /** The direction of the left rear encoder
-     *  Default Value: Encoder.REVERSE */
+     * Default Value: Encoder.REVERSE */
     public static double leftRearEncoderDirection = Encoder.REVERSE;
 
     /** The direction of the right rear encoder
-     *  Default Value: Encoder.FORWARD */
+     * Default Value: Encoder.FORWARD */
     public static double rightRearEncoderDirection = Encoder.FORWARD;
 }
