e787004bca953cf60b4b76df8c830dd43cc2deef
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-01-20 16:31:01 -0600

    com.pedropathing:beta:1.0.7-beta5 - pinpoint testing - update javadoc for localizer constants

--- src/main/java/com/pedropathing/localization/constants/PinpointConstants.java
+++ src/main/java/com/pedropathing/localization/constants/PinpointConstants.java
@@ -12,21 +12,52 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class PinpointConstants {
 
-    /** The Y Offset of the Forward Encoder (Deadwheel) from the center of the robot
-     * @value 1 */
+    /** The Y Offset of the Forward Encoder (Deadwheel) from the center of the robot in DistanceUnit
+     * @see #distanceUnit
+     * Default Value: 1 */
     public static double forwardY = 1;
 
-    /** The X Offset of the Strafe Encoder (Deadwheel) from the center of the robot
-     * @value -2.5 */
+    /** The X Offset of the Strafe Encoder (Deadwheel) from the center of the robot in DistanceUnit
+     * @see #distanceUnit
+     * Default Value: -2.5 */
     public static double strafeX = -2.5;
 
+    /** The Unit of Distance that the Pinpoint uses to measure distance
+     * Default Value: DistanceUnit.INCH */
     public static DistanceUnit distanceUnit = DistanceUnit.INCH;
+
+    /** The name of the Pinpoint in the hardware map (name of the I2C port it is plugged into)
+     * Default Value: "pinpoint" */
     public static String hardwareMapName = "pinpoint";
+
+    /** Use custom yaw scalar for the Pinpoint (overrides the calibration of the Pinpoint
+     * Default Value: false */
     public static boolean useYawScalar = false;
+
+    /** Custom Yaw Scalar for the Pinpoint (overrides the calibration of the Pinpoint
+     * @see #useYawScalar
+     * Default Value: 1.0 */
     public static double yawScalar = 1.0;
+
+    /** Use custom encoder resolution for the Pinpoint
+     * Default Value: false */
     public static boolean useCustomEncoderResolution = false;
+
+    /** The Encoder Resolution for the Pinpoint. Used if useCustomEncoderResolution is false
+     * @see #useCustomEncoderResolution
+     * Default Value: GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD */
     public static GoBildaPinpointDriver.GoBildaOdometryPods encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+
+    /** The Encoder Resolution for the Pinpoint. Only used if useCustomEncoderResolution is true
+     * @see #useCustomEncoderResolution
+     * Default Value: 13.26291192 */
     public static double customEncoderResolution = 13.26291192;
+
+    /** The Encoder Direction for the Forward Encoder (Deadwheel)
+     * Default Value: GoBildaPinpointDriver.EncoderDirection.REVERSED */
     public static GoBildaPinpointDriver.EncoderDirection forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
+
+    /** The Encoder Direction for the Strafe Encoder (Deadwheel)
+     * Default Value: GoBildaPinpointDriver.EncoderDirection.FORWARD */
     public static GoBildaPinpointDriver.EncoderDirection strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
 }
